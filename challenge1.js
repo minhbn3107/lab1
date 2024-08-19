4f89ad659f85240e4f44dddcc2c05926ceeb3341
bnminh <minhbn3107>
2024-08-19 14:42:33 +0700

    challenge1

---
+++ challenge1.js
@@ -0,0 +1,23 @@
+// Data 1
+const markWeight1 = 78;
+const markHeight1 = 1.69;
+const johnWeight1 = 92;
+const johnHeight1 = 1.95;
+
+const markBMI1 = markWeight1 / (markHeight1 * markHeight1);
+const johnBMI1 = johnWeight1 / (johnHeight1 * johnHeight1);
+
+const markHigherBMI1 = markBMI1 > johnBMI1;
+console.log(markHigherBMI1);
+
+// Data 2
+const markWeight2 = 95;
+const markHeight2 = 1.88;
+const johnWeight2 = 85;
+const johnHeight2 = 1.76;
+
+const markBMI2 = markWeight2 / (markHeight2 * markHeight2);
+const johnBMI2 = johnWeight2 / (johnHeight2 * johnHeight2);
+
+const markHigherBMI2 = markBMI2 > johnBMI2;
+console.log(markHigherBMI2);
