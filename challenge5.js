2fad1def2643154473cb6acf7518d456ef9d65f4
bnminh <minhbn3107>
2024-08-26 13:53:02 +0700

    challenge5

---
+++ challenge5.js
@@ -0,0 +1,18 @@
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+const avgDolphins1 = calcAverage(44, 23, 71);
+const avgKoalas1 = calcAverage(65, 54, 49);
+
+console.log(`Dolphins' Average Score: ${avgDolphins1}`);
+console.log(`Koalas' Average Score: ${avgKoalas1}`);
+
+const checkWinner = (avgDolphins, avgKoalas) => {
+    if (avgDolphins >= 2 * avgKoalas) {
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas >= 2 * avgDolphins) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    } else {
+        console.log("No team wins.");
+    }
+};
+checkWinner(avgDolphins1, avgKoalas1);
